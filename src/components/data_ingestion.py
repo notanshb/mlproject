f6e477b3c5d0d78b4ebbd7b925c76da3e3655b84
Ansh Bhardwaj <bhardwajansh@outlook.com>
2024-09-22 01:13:01 +0530

    data transformation completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformationConfig, DataTransformation
+
 @dataclass #directly define class varaible w/o using __init__; use only when u need just variables and not methods
 class DataIngestionConfig:
     '''
@@ -44,10 +46,14 @@ class DataIngestion:
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
+        
         except Exception as e:
             raise CustomException(e,sys)
         
 
 if __name__== "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
