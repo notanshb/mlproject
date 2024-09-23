aaac99bd0f45be29c284e7f99c2444f8c7aa1dc7
Ansh Bhardwaj <bhardwajansh@outlook.com>
2024-09-23 23:13:09 +0530

    model trainer along with hyperparameter tuning added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformationConfig, DataTransformation
 
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
+
 @dataclass #directly define class varaible w/o using __init__; use only when u need just variables and not methods
 class DataIngestionConfig:
     '''
@@ -56,4 +58,7 @@ if __name__== "__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
